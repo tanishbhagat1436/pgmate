899c24fae8e12bc6a4298e6b20ea0d89d1503493
Tanish P Bhagat <tanishbhagat1436@gmail.com>
2026-08-19 11:58:46 +0530

    Update WebMvcConfig.java

--- src/main/java/com/PGMATE/demo/config/WebMvcConfig.java
+++ src/main/java/com/PGMATE/demo/config/WebMvcConfig.java
@@ -1,14 +1,21 @@
 package com.PGMATE.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Automatically redirects root URL ("/") to your frontend home page
         registry.addViewController("/").setViewName("forward:/Frontend/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/", "classpath:/static/Frontend/");
     }
 }
